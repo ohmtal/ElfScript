f48bcdea4920420cea584a6396effb4af32f3b61
XXTH <tom@mxplus.de>
2026-08-02 15:41:56 +0200

    added print to ElfScript

--- ElfScript/console/consoleFunctions.cpp
+++ ElfScript/console/consoleFunctions.cpp
@@ -2068,6 +2068,27 @@ DefineEngineStringlyVariadicFunction( echo, void, 2, 0, "( string message... ) "
    ret[0] = 0;
 }
 
+DefineEngineStringlyVariadicFunction( print, void, 2, 0, "( string message... ) "
+"@brief Logs a message to the std out \n"
+"Concatenates all given arguments to a single string and prints the string to the std out.\n"
+"A newline is added automatically after the text.\n"
+"@param message Any number of string arguments." )
+{
+      U32 len = 0;
+      S32 i;
+      for(i = 1; i < argc; i++)
+            len += dStrlen(argv[i]);
+
+      char *ret = Con::getReturnBuffer(len + 1);
+      ret[0] = 0;
+      for(i = 1; i < argc; i++)
+            dStrcat(ret, argv[i], (U64)(len + 1));
+
+      // Con::printf("%s", ret);
+      dPrintf("%s", ret);
+      ret[0] = 0;
+}
+
 //-----------------------------------------------------------------------------
 
 DefineEngineStringlyVariadicFunction( warn, void, 2, 0, "( string message... ) "
