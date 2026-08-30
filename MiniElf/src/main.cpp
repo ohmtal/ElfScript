a6ac8725cb972467c5a3de50d0ccb642fc21637c
XXTH <tom@mxplus.de>
2026-08-30 02:24:31 +0200

    added example functions

--- MiniElf/src/main.cpp
+++ MiniElf/src/main.cpp
@@ -2,14 +2,17 @@
 // Copyright (c) 2026 Thomas Hühn (XXTH)
 // SPDX-License-Identifier: MIT
 //-----------------------------------------------------------------------------
-// baseElf Main source
+// MiniElf Main source
 //-----------------------------------------------------------------------------
 #include <cstdio>
 
 // ---- ElfScript
-#include "main/engineGlue.h"
-#include "console/script.h"
+#include "main/engineGlue.h"    // << to init/shutdown
+#include "console/script.h"     // << to load a script
+#include "console/engineAPI.h"  // << to bind functions
 
+
+// using my own arg parser for script file only
 int argParser(int argc, char* argv[]) {
 
     // pass to script in TGE/OGE3D  it is called Game::!,
@@ -32,24 +35,34 @@ int argParser(int argc, char* argv[]) {
     } //for ...
     return 0;
 }
+// ----------------------------------------------------------------------------
+// Example how to bind a C function
+DefineEngineFunction(MyScriptFunction, void, (), ,"A comment about this function") {
+    printf("Calling c printf but you can call everything here\n");
+}
 
+DefineEngineFunction(MyScriptFunctionWithParameter, void, (bool aParameter), (true),
+        "We can add\n"
+        "multiline comments\n") {
+
+    // S32 is a 32bit signed integer :
+    printf("Your parameter is %d\n", (S32)aParameter);
+}
 // ----------------------------------------------------------------------------
 int main(int argc, char* argv[]) {
     engineGlue::silent = true;
     engineGlue::ScriptFile = "";
     argParser(argc, argv);
-
     if (engineGlue::ScriptFile.isEmpty()) {
         printf("\n\nUsage: %s scriptfilename.elf\n\n", argv[0] );
         return 0;
     }
-    // Con::infof("Init ElfScript.....");
     engineGlue::init(nullptr ); // nullptr => we use default logger
 
-    // Con::infof("loading script %s", engineGlue::ScriptFile.c_str());
+    // ... some code here
     Con::executeFile(engineGlue::ScriptFile);
+    // and some code here .....
 
-    // Con::infof("Shutdown ElfScript ....");
     engineGlue::shutDown();
     return 0;
 }
